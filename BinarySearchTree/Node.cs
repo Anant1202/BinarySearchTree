0a56a006cb374f763ae3bf95819f532bc81a2c0e
Anant1202 <anantsingh1202@gmail.com>
2022-05-29 20:13:18 +0530

    Added:Add elements

---
+++ BinarySearchTree/Node.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    public class Node<T> where T : IComparable
+    {
+        public T nodeData;
+        public Node<T> Leftc;
+        public Node<T> Rightc;
+        public Node(T data)
+        {
+            this.nodeData = data;
+            this.Leftc = null;
+            this.Rightc = null;
+        }
+        int leftCount = 0, rightCount = 0;
+        public void Add(T item)
+        {
+            T current = nodeData;
+            if ((current.CompareTo(item)) > 0)
+            {
+                if (Leftc == null)
+                {
+                    Leftc = new Node<T>(item);
+                }
+                else
+                {
+                    Leftc.Add(item);
+                }
+            }
+            else
+            {
+                if (Rightc == null)
+                {
+                    Rightc = new Node<T>(item);
+                }
+                else
+                {
+                    Rightc.Add(item);
+                }
+            }
+        }
+
+        public void Display()
+        {
+            if (Leftc != null)
+            {
+                leftCount++;
+                Leftc.Display();
+            }
+            Console.WriteLine(this.nodeData);
+            if (Rightc != null)
+            {
+                rightCount++;
+                Rightc.Display();
+            }
+        }
+    }
+}
+
+
--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -6,7 +6,11 @@ namespace BinarySearchTree
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Binary Search Program");
+            Console.WriteLine("Welcome to Binary Search Tree Program");
+            Node<int> obj = new Node<int>(56);
+            obj.Add(30);
+            obj.Add(70);
+            obj.Display();
         }
     }
 }
