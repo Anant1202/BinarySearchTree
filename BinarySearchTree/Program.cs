d4357258ffc0154aabe9495a1ffc1b525ae70a3a
Anant1202 <anantsingh1202@gmail.com>
2022-05-29 16:53:05 +0530

    Welcome to Binary Search Tree Program

---
+++ BinarySearchTree/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BinarySearchTree
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Binary Search Program");
+        }
+    }
+}
+
